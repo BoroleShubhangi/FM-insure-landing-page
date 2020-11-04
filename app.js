26475eeda8cb96c2ac5fa3e48b828f1f9cb39c62
shubhangi Borole <shubhangiborole200@gmail.com>
2020-11-04 13:21:06 +0530

    last commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var menu= document.getElementById("toggle-menu");
+var menu= document.getElementById("toggle_menu");
 var nav=document.getElementById("nav_menu");
 
 menu.addEventListener("click",function(){
-    console.log("click");
 this.classList.toggle("active");
 nav.classList.toggle('open');
 })
